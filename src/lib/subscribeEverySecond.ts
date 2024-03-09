783625301bbc935fbd5a067d77b7c9cd04417ba5
Kirill Loskutov <kl@sinker.ru>
2024-03-09 21:15:01 +0100

    Fix the issue with countdown stuttering sometimes

--- src/lib/subscribeEverySecond.ts
+++ src/lib/subscribeEverySecond.ts
@@ -4,9 +4,9 @@ export const subscribeEverySecond = (cb: (ms: number) => void) => {
   let timeoutHandle: ReturnType<typeof setTimeout> | undefined = undefined
   const loop = () => {
     cb(Date.now())
-    timeoutHandle = setTimeout(loop, getMsToNextSecond())
+    timeoutHandle = setTimeout(loop, getMsToNextSecond()+10)
   }
-  timeoutHandle = setTimeout(loop, getMsToNextSecond())
+  timeoutHandle = setTimeout(loop, getMsToNextSecond()+10)
 
   return () => {
     clearTimeout(timeoutHandle)
